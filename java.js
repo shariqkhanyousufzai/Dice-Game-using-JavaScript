c04b1956d7d89a3c93fad40f09f82f6b8c398089
shariqkhanyousufzai@outlook.com <shariqkhanyousufzai@outlook.com>
2018-07-13 08:05:45 -0700

    Till-Video-15

---
+++ java.js
@@ -0,0 +1,138 @@
+// // console.log ("hello");
+// // var age = 'what';
+// // console.log(age);
+// // var working = 'i am working fine bro why worried?';
+// // console.log( age + 'happen?' + working);
+
+
+
+// // // lecture : Operators!
+// // var birthDay = 2016 - 26;
+// // // here * operator will perform first
+// // birthDay  = 2016 - 2 * 2; // operator perform right to left
+// // birthDay = (2016 - 2) * 2; 
+// // var six = 2016;
+// // birthDay  = six - 2 * 2;
+// // console.log(birthDay);
+// // var johnAge = 30;
+// // var allenAge = 30;
+// // johnAge = allenAge =  (3 + 5) * 4 - 6 + 4;
+// // johnAge++;
+// // allenAge+=5;
+// // allenAge*=2;
+
+// // console.log(johnAge + ' ' + allenAge ); 
+
+// //--------------------------End-of-lecture-8-----
+// /*
+// if(23 == "23"){
+//     console.log("hello world");
+// }
+// */
+// //-------------------------End - of - lecture 9---------
+
+
+// // var age = 25;
+
+// // if(age < 20){
+// //     console.log("john is a man");
+// // }else{
+// //     console.log('he is a fuckin teenager');
+// // }
+
+
+// // // switch case 
+// // john = prompt('what you want');
+
+// //     switch (john){
+// //         case 'teacher':
+// //             console.log('he was teaching');
+// //             break;
+
+// //         case 'cop' :
+// //             console.log('dealing with cops');
+// //             break;
+// //         case 'gank' :
+// //             console.log('deals with gank');
+// //             break;
+// //         default: 
+// //             console.log('fuck off');
+        
+// //     }
+
+// // coding challenge
+// // johnHeight = prompt('John Height');
+// // friendHeight = prompt('Friend Height');
+// // johnAge = prompt('john Age');
+// // friendAge = prompt('friend Age');
+// // thirdPlayerHeight = prompt("enter height third");
+// // thirdPlayerAge = prompt("enter age third");
+// // calculationOfJohn = (johnHeight*5)+ johnAge ; 
+// // calculationOfFriend = (friendHeight*5) + friendAge;
+// // calculationOfthird = (thirdPlayerHeight*5) + thirdPlayerAge;
+
+// // if(calculationOfJohn > calculationOfFriend && calculationOfJohn>calculationOfthird) {
+// //     console.log('John wins because his total score is ' + calculationOfJohn + ' And  friend // third score is ' + calculationOfFriend + calculationOfthird );
+// // }else if(calculationOfJohn < calculationOfFriend && calculationOfthird<calculationOfFriend){
+// //     console.log('Friend wins because his total score is ' + calculationOfFriend + ' And John // third score is ' + calculationOfJohn + calculationOfthird);
+// // }else if (calculationOfthird > calculationOfJohn && calculationOfthird > calculationOfFriend){
+// //     console.log('Third  wins because his total score is ' + calculationOfthird + ' And John // friend score is ' + calculationOfJohn + calculationOfFriend);
+    
+// // }
+   
+// // var khan = 'shariq';
+// // if(khan){
+// //     console.log('ok');
+// // }else{
+// //     console.log('no');
+// // }
+// // if(khans){
+// //     console.log('ok');
+// // }else{
+// //     console.log('no');
+// // }
+ 
+// // var khan = 19;
+// // if(19){
+// //     console.log('shariq is khan');
+// // }else if (18){
+// //     console.log('shariq is ok');
+// // }else if (19){
+// //     console.log('basit is khan');
+// // }
+// // if(19){
+// //     console.log('shariq is khan');
+// // }else if (18){
+// //     console.log('shariq is ok');
+// // }else if (19){
+// //     console.log('basit is khan');
+// // }
+
+// // function calculateAge(birthOfYears){
+// //    var age = 2018 - birthOfYears;
+// //    return age;
+// // }
+
+// // var john = calculateAge(1996);
+// // console.log(john);
+
+// // function canvasMirror(m,x){
+// //     var f = m / x;
+// //     return f;
+// // }
+// // var mirror  = canvasMirror(2,2);
+// // console.log(mirror); 
+
+// var namess = ['John','Jane','Mark'];
+// var names = new Array(1990,1991,1992);
+// namess[1] = 'ben';
+
+// namess.push ('blue');
+// namess.pop ();
+// namess.pop ();
+// namess.unshift('Mr');
+// namess.shift();
+// namess.shift();
+// // console.log(name[6]);
+
+// console.log(namess);
